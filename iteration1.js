663ad6cfd70e7bbfd70fc101340d4b7cf4b6af1d
Ivan Jimenez <chiklete@gmail.com>
2021-10-28 09:29:10 +0200

    Iteration#1

---
+++ iteration1.js
@@ -0,0 +1,16 @@
+//Iteración #1: Arrows
+
+let a = 5;
+let b = 10;
+
+// 1.1 Ejecuta esta función sin pasar ningún parametro
+let sumaAB = () => a + b;
+console.log(sumaAB())
+
+// 1.2 Ejecuta esta función pasando un solo parametro
+let sumaCB = c => c + b;
+console.log(sumaCB(3))
+
+// 1.3 Ejecuta esta función pasando dos parametros
+let sumaCD = (c, d) => c + d;
+console.log(sumaCD(3, 7))
