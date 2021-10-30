e575c2fc7312f33760aef515e9cda38bad4d5fdf
Ivan Jimenez <chiklete@gmail.com>
2021-10-30 14:21:53 +0200

    Iteration#3-2

--- iteration3.js
+++ iteration3.js
@@ -29,5 +29,5 @@ console.log(allToys)
 //3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 pero sin editar el array inicial. De nuevo, usando spread operatos.
 console.log('//Iteración #3: 3.5')
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
-let [rojo, , amarillo, verde, naranja]=colors;
-console.log(rojo, amarillo, verde,naranja)
+let newColores = [...colors.slice(0,2), ...colors.slice(3)];
+console.log(newColores)
